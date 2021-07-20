37224ac694c95da338e100c26d6bdbb9d92177ee
rovm <rovm92@naver.com>
2021-07-20 23:31:06 +0900

    포스트 목록 조회 API 연동하기
    user.username 오류 찾는중

--- react/blog/blog-fontend/src/pages/PostListPage.js
+++ react/blog/blog-fontend/src/pages/PostListPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import HeaderContainer from '../components/common/HeaderContainer';
+import PostListContainer from '../components/posts/PostListContainer';
 
 const PostListPage = () => {
     return (
         <>
             <HeaderContainer/>
-            <div>안녕하세요</div>
+            <PostListContainer/>
         </>
     );
 };
