ea828e7b52b300a58b812dbd19be57ef473689ff
rovm <rovm92@naver.com>
2021-07-24 22:57:00 +0900

    Html 필터링하기

--- react/blog/blog-backend/src/api/posts/posts.ctrl.js
+++ react/blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,8 +1,34 @@
 import Post from '../../models/post';
 import mongoose from 'mongoose';
 import Joi from '@hapi/joi';
+import sanitizeHtml from 'sanitize-html';
 
 const {ObjectId} = mongoose.Types;
+
+const sanitizeOption = {
+  allowedTags: [
+    'h1',
+    'h2',
+    'b',
+    'i',
+    'u',
+    's',
+    'p',
+    'ul',
+    'ol',
+    'li',
+    'blockqute',
+    'a',
+    'img',
+  ],
+  allowedAttribute: {
+    a: ['href', 'name', 'target'],
+    img: ['src'],
+    li: ['class'],
+  },
+  allowedSchemes: ['data', 'http'],
+};
+// https://www.npmjs.com/package/sanitize-html
 
 export const checkObjectId = (ctx, next) => {
   const {id} = ctx.params;
@@ -35,7 +61,7 @@ export const write = async ctx => {
   const post = new Post({
     title,
     body,
-    tags,
+    tags: removeHtmlAndShorten(body, sanitizeOption),
     user: ctx.state.user,
   });
   try{
@@ -45,6 +71,14 @@ export const write = async ctx => {
     ctx.throw(500, e);
   }
 }
+
+//html을 없애고 내용이 너무 길면 200자로 제한하는 함수
+const removeHtmlAndShorten = body => {
+  const filtered = sanitizeHtml(body, {
+    allowedTags: [],
+  });
+  return filtered.length < 200 ? filtered : `${filtered.slice(0, 200)}...`;
+};
 
 export const list = async ctx => {
   //query는 문자열이기 때문에 숫자로 변환해 주어야 합니다.
@@ -68,8 +102,7 @@ export const list = async ctx => {
     ctx.set('Last-Page', Math.ceil(postCount/10));
     ctx.body = posts.map(post => ({
       ...post,
-      body:
-        post.body.length < 5 ? post.body : `${post.body.slice(0, 5)}...`,
+      body: removeHtmlAndShorten(post.body),
     }));
   } catch(e){
     ctx.throw(500, e);
@@ -116,6 +149,12 @@ export const update = async ctx => {
     ctx.status = 400; // Bad Request
     ctx.body = result.error;
     return;
+  }
+
+  const nextData = {...ctx.request.body}; //객체를 복사하고
+  //body 값이 주어졌으면 html 필터링
+  if(nextData.body){
+    nextData = sanitizeHtml(nextData.body, sanitizeOption);
   }
 
   try{
