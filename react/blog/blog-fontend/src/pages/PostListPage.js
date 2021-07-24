789875d9e84d3ae95d90db021da2820dd98fbd81
rovm <rovm92@naver.com>
2021-07-24 23:35:14 +0900

    페이징 구현

--- react/blog/blog-fontend/src/pages/PostListPage.js
+++ react/blog/blog-fontend/src/pages/PostListPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeaderContainer from '../components/common/HeaderContainer';
 import PostListContainer from '../components/posts/PostListContainer';
+import PaginationContainer from '../components/posts/PaginationContainer';
 
 const PostListPage = () => {
     return (
         <>
             <HeaderContainer/>
             <PostListContainer/>
+            <PaginationContainer/>
         </>
     );
 };
